c59d14aaa4ab21a1f1573062c93235fb70c9e336
DumakIt <odumak4411@gmail.com>
2023-02-06 21:36:15 +0900

    23.02.06 Section19 서버에 이미지 파일 업로드

---
+++ class/pages/section19/19-01-image-upload/index.tsx
@@ -0,0 +1,44 @@
+import { gql, useMutation } from "@apollo/client";
+import { ChangeEvent, useState } from "react";
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from "../../../src/commons/types/generated/types";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [imageUrl, setImageUrl] = useState("");
+  const [uploadFile] = useMutation<
+    Pick<IMutation, "uploadFile">,
+    IMutationUploadFileArgs
+  >(UPLOAD_FILE);
+
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>
+  ): Promise<void> => {
+    const file = event.target.files?.[0]; //  배열로 들어오는 이유: <input type="file" multiple /> 이거 일때 여러개의 이미지 가능
+    console.log(file);
+
+    const result = await uploadFile({
+      variables: {
+        file,
+      },
+    });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+
+  return (
+    <>
+      <input type="file" onChange={onChangeFile} />
+      <img src={`https://storage.googleapis.com/${imageUrl}`} />
+    </>
+  );
+}
---
+++ class/pages/section19/19-02-image-ref/index.tsx
@@ -0,0 +1,63 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, useRef } from "react";
+import type { ChangeEvent } from "react";
+
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from "../../../src/commons/types/generated/types";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [imageUrl, setImageUrl] = useState("");
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  const [uploadFile] = useMutation<
+    Pick<IMutation, "uploadFile">,
+    IMutationUploadFileArgs
+  >(UPLOAD_FILE);
+
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>
+  ): Promise<void> => {
+    const file = event.target.files?.[0]; //  배열로 들어오는 이유: <input type="file" multiple /> 이거 일때 여러개의 이미지 가능
+    console.log(file);
+
+    const result = await uploadFile({
+      variables: {
+        file,
+      },
+    });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+
+  const onClickImage = (): void => {
+    fileRef.current?.click();
+  };
+
+  return (
+    <>
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "gray" }}
+        onClick={onClickImage}
+      >
+        이미지 선택
+      </div>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        onChange={onChangeFile}
+        ref={fileRef}
+      />
+      <img src={`https://storage.googleapis.com/${imageUrl}`} />
+    </>
+  );
+}
