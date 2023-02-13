b5a4436cff536142b7031aead29d5ad92bf75449
DumakIt <odumak4411@gmail.com>
2023-02-13 23:47:46 +0900

    23.02.13 로그인시 접속 승인코드 받기 공부 내역 업데이트

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -5,6 +5,8 @@ import {
   ApolloLink,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../commons/stores";
 
 interface IApolloSettingProps {
   children: JSX.Element;
@@ -13,8 +15,13 @@ interface IApolloSettingProps {
 const GLOBAL_STATE = new InMemoryCache();
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
+  const [accessToken] = useRecoilState(accessTokenState);
+
   const uploadLink = createUploadLink({
     uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
   });
 
   const client = new ApolloClient({
